9c64bbc4cf77943c43d37b6786edbbaa7851d308
haohaogit <menghaohao_dz@163.com>
2018-12-22 20:28:19 +0800

    haohao spring_boot demo

---
+++ initial/src/main/java/hello/Application.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @CopyRight By zcy.qyg.cn
+ * @ClassName Application
+ * @Description TODO
+ * @Author DongQing
+ * @Date 2018-12-22 20:24
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ initial/src/main/java/hello/Greeting.java
@@ -0,0 +1,26 @@
+package hello;
+
+/**
+ * @CopyRight By zcy.qyg.cn
+ * @ClassName Greeting
+ * @Description TODO
+ * @Author DongQing
+ * @Date 2018-12-22 20:21
+ **/
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
